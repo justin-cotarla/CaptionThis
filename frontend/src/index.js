4f7f43f7a1e1738a9637a9e81246806212d48442
ZacharyBys <zachary.bys@gmail.com>
2018-02-03 16:35:02 -0500

    [#12] Set index to show App instead of test component

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,5 +5,5 @@ import App from './App';
 import Moment from './components/Moment'
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<Moment />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
