a200c75a2644cd724f0068a26a2f60098da4fdd5
ZacharyBys <zachary.bys@gmail.com>
2018-02-03 16:39:33 -0500

    [#12] Travis doesn't like unused variables

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Moment from './components/Moment'
+//import Moment from './components/Moment'
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
