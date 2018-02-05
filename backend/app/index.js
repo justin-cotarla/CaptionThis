4078c101e081a21ccab61759823aeda733662997
Jay Wreh <jeremiahdavid.wreh@gmail.com>
2018-02-04 21:54:03 -0500

    [#29] Set up folder and file structure for endpoints (#30)

--- backend/app/index.js
+++ backend/app/index.js
@@ -1,14 +1,21 @@
 import Hapi from 'hapi';
+import routes from './routes';
 
-const server = new Hapi.Server({ port: 8000, host: 'backend' });
-
-server.route({
-  method: 'GET',
-  path: '/api/hello',
-  handler: (request, h) => h.response('Hello World!'),
+const server = new Hapi.Server({
+	port: 8000,
+	host: 'backend'
 });
 
+server.route({
+	method: 'GET',
+	path: '/api/hello',
+	handler: (request, reply) => 'Hello World'
+});
+
+//Add all routes to server
+server.route(routes);
+
 server.start((err) => {
-  if (err) throw err;
-  console.log(`Server running at: ${server.info.uri}`);
+	if (err) throw err;
+	console.log(`Server running at: ${server.info.uri}`);
 });
