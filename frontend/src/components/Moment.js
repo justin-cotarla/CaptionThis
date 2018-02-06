0ad85b0e232fee124f35bcafce3ece6ce391ec73
ZacharyBys <zachary.bys@gmail.com>
2018-02-06 17:20:02 -0500

    [#23] Added basic parameters to moment component

--- frontend/src/components/Moment.js
+++ frontend/src/components/Moment.js
@@ -1,15 +1,12 @@
 import React, { Component } from 'react';
-import sample from '../resources/sample-moment.jpg';
 import '../styles/Moment.css';
 
 class Moment extends Component {
     render() {
       return (
         <div className="Moment">
-          <header className="App-header">
-            <img src={sample} className="photo" alt="logo" />
-            <h1 className="Moment-title">Caption this for me people!</h1>
-          </header>
+            <img src={this.props.Image} className="photo" alt="logo" />
+            <div className="Moment-text-container"><h1 className="Moment-title">{this.props.Description}</h1></div>
         </div>
       );
     }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,8 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Moment from './components/Moment';
+import sample from './resources/sample-moment.jpg';
 //import Moment from './components/Moment'
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//ReactDOM.render(<App id="app"/>, document.getElementById('root'));
+ReactDOM.render(<Moment Description={"Find me a cool caption!"} Image={sample} />, document.getElementById('root'));
 registerServiceWorker();
+
