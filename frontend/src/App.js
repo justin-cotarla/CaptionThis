1012f83606343f4e0cb76109cfbd2cda1a2adb26
Krish <thekrish360@hotmail.com>
2018-02-07 17:29:56 -0500

    [#19] Started shell for moment creation page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import LandingPage from './pages/LandingPage';
 import './App.css';
+import MomentCreation from './pages/MomentCreation';
 
 class App extends Component {
   render() {
     return (
-      <LandingPage />
+     // <LandingPage />
+        <MomentCreation />
     );
   }
 }
---
+++ frontend/src/pages/MomentCreation.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+
+class LandingPage extends Component {
+
+    render() {
+        
+    }
+}
+  
+  export default MomentCreation;
