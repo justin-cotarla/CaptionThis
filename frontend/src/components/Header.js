fbc8a6c4a86866e92f6d073fa4a0f1addadbd27d
ZacharyBys <zachary.bys@gmail.com>
2018-02-11 16:52:46 -0500

    [#51] Changes for review

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,17 +2,20 @@ import React, { Component } from 'react';
 
 class Header extends Component{
     render() {
-        var size = this.props.textSize;
-        var headerStyle = {
-            fontSize: '12px',
-          };
-
-        if (size === 2) {
-            headerStyle.fontSize = '16px'
-        } else if (size === 3) {
-            headerStyle.fontSize = '20px'
-        } else if (size === 4) {
-            headerStyle.fontSize = '24px'
+        let size = this.props.textSize;
+        let headerStyle = { fontSize: '10px', }; 
+        switch (size) { 
+            case 2: 
+                headerStyle.fontSize = '16px'; 
+                break; 
+            case 3: 
+                headerStyle.fontSize = '20px'; 
+                break; 
+            case 4: 
+                headerStyle.fontSize = '24px'; 
+                break; 
+            default: 
+                headerStyle.fontSize = '12px'; 
         }
 
         return (
