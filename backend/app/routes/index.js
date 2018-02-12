4fd38ff5056a1caa6f798f857a3b1d7c85a7322d
Justin Cotarla <justin.cotarla@gmail.com>
2018-02-12 15:49:12 -0500

    [#37] implement registration route

--- backend/app/routes/index.js
+++ backend/app/routes/index.js
@@ -1,5 +1,11 @@
 import moments from './moments';
 import captions from './captions';
 import users from './users';
+import register from './auth/register';
 
-export default [].concat(moments, captions, users);
+export default [].concat(
+    moments,
+    captions,
+    users,
+    register,
+);
