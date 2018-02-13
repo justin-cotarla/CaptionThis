3821e12946eb191444bee85e24705d8d3af8064d
Krish <thekrish360@hotmail.com>
2018-02-13 18:06:47 -0500

    [#55] Link backend to frontend

---
+++ frontend/src/pages/MomentCreationPage.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import '../styles/CreateMoment.css';
+
+class MomentCreation extends Component {
+
+    constructor() {
+        super();
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        const data = new FormData(event.target);
+    
+    fetch('http://localhost:16085/api/moments/', {
+      method: 'PUT',
+      body: data,
+    });
+  }
+    render(){
+        return(
+            <div className = "CreateMoment">
+                <form onSubmit={this.handleSubmit} encType="multipart/form-data" noValidate>
+                    <div className = "content-container">
+                        <label className="label">Title</label>
+                        <input name="title" type="text" className="title" required/>
+                    </div>
+                    <div className = "content-container">
+                        <input type="file" name="file" required></input>
+                    </div>
+                    <div className = "content-container">
+                        <label className="label">Description</label>
+                        <textarea name="description" rows="8" cols="90"></textarea>
+                    </div>
+                    <div className = "content-container">
+                        <input type="submit" className="button"></input>
+                    </div>
+                </form>
+            </div>
+        );
+    }
+}
+  
+export default MomentCreation;
