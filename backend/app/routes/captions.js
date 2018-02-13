024c9c455f3fb1537ec57d5c2a434540ab1650c2
Derek Yu <derekyu624@hotmail.com>
2018-02-13 03:47:30 -0500

    [#60] Created a PUT endpoint for caption creations.

--- backend/app/routes/captions.js
+++ backend/app/routes/captions.js
@@ -1,3 +1,33 @@
-//Add your /api/captions endpoint here
+import databaseUtil from '../utility/DatabaseUtil';
 
-export default [];
+const createCaption = {
+    method: 'PUT',
+    path: '/api/captions/',
+    handler: (request, reply) => {
+        // Get the caption from request
+        const { content } = request.payload;
+        const voteCount = 0; // Count of newly created caption is 0
+        const selected = 0; // 0 for not seleted, 1 for selected
+        const userId = 1; // Placeholder user ID
+        const momentId = 1; // Placeholder moment ID
+
+        // Check if the caption content is valid
+        if (content === '') {
+            return reply.response({ code: 2 }).code(400);
+        }
+
+        // Create db query
+        const query = 'INSERT INTO CAPTION (CONTENT, VOTE_COUNT, SELECTED, USER_ID, MOMENT_ID) VALUES (?, ?, ?, ?, ?)';
+        return databaseUtil
+            .sendQuery(query, [content, voteCount, selected, userId, momentId])
+            .then(() => reply.response({ code: 1 }).code(200))
+            .catch((error) => {
+                console.log(error);
+                return reply.response({ code: 3 }).code(500);
+            });
+    },
+};
+
+export default [
+    createCaption,
+];
