3293aa0c92dc996c47c27fb14886e5a2bc03420c
tehblasian <jeremiahdavid.wreh@gmail.com>
2018-02-13 19:55:56 -0500

    [#27] Forgot to add the limit to the query

--- backend/app/routes/moments.js
+++ backend/app/routes/moments.js
@@ -24,7 +24,7 @@ const getMomentsByDate = {
         SELECT MOMENT.ID AS MOMENT_ID, 
         IMG_URL, DESCRIPTION, DATE_ADDED, USER.USERNAME, 
         USER.ID AS USER_ID FROM MOMENT 
-        JOIN USER ON MOMENT.USER_ID = USER.ID
+        JOIN USER ON MOMENT.USER_ID = USER.ID LIMIT ?
         `;
         return databaseUtil.sendQuery(query, [limit]).then((result) => {
             const moments = result.rows.map(moment => ({
