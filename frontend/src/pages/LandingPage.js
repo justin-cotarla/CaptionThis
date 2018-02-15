0497a840925f43bc6fd5d124bf5c02ab26a81eed
Justin Cotarla <justin.cotarla@gmail.com>
2018-02-14 21:05:55 -0500

    [#86] add CAPTION_VOTE table in schema

--- frontend/src/pages/LandingPage.js
+++ frontend/src/pages/LandingPage.js
@@ -1,64 +1,93 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import Cookies from 'universal-cookie';
+import base64url from 'base64-url';
+
 import MomentsList from '../components/MomentsList';
 import ScrollApp from '../components/ScrollerComponents'
 import Header from '../components/Header';
 import '../styles/LandingPage.css';
 
 class LandingPage extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      moments: null,
-      error: null,
+    constructor(props){
+        super(props);
+        this.state = {
+            moments: null,
+            error: null,
+            redirect: null,
+            user: null,
+        };
     };
-  };
 
-  componentWillMount(){
-    axios.get(`http://${process.env.REACT_APP_IP}:16085/api/moments`).then(response => {
-      let moments = response.data.moments;
-      this.setState({
-        moments,
-      });
-    }).catch(error => {
-      console.log(error);
-      this.setState({
-        error: 'Oops! Something went wrong...'
-      });
-    });
-  };
-
-  render() {
-    const moments = this.state.moments;
-    const error = this.state.error;
-
-    // Return an error message if moments could not be loaded
-    if (error) {
-      return <div className="landing-page-container">
-        <Header textSize={4} text={error} />
-      </div>
+    loadUser = () => {
+        const cookies = new Cookies();
+        const token = cookies.get('token');
+        if (token !== undefined) {
+           this.setState({
+               user: base64url.decode(token.split('.')[1])
+           });
+        }
     }
 
-    return (
+    componentWillMount(){
+        this.loadUser();
+        axios.get(`http://${process.env.REACT_APP_IP}:16085/api/moments`)
+        .then(response => {
+            let moments = response.data.moments;
+            this.setState({
+                moments,
+            });
+        })
+        .catch(error => {
+            console.log(error);
+            this.setState({
+                error: 'Oops! Something went wrong...'
+            });
+        });
+    };
+    
+    render() {
+        const moments = this.state.moments;
+        const error = this.state.error;
         
-        <body bgcolor="#303030">
-          <div id="header">
-            <div id="left">
-              <img id="personIcon" alt="Pic Loading..." src="personIcon.png" width="20"/>
+        // Return an error message if moments could not be loaded
+        if (error) {
+            return <div className="landing-page-container">
+                <Header textSize={4} text={error} />
             </div>
-            <div id="right">
-              <p align="right">
-                <button onclick="location.href='http://google.com';" align="right" width= "100"value="Sign Up | Sign In" style={{border:"1px",height:"40px", width:"100px"}} />
-              </p>
+        }
+        
+        return (
+            <div>
+                <div className="header">
+                    <div className="profile-button">
+                        <img
+                            alt="Profile"
+                            src="personIcon.png"
+                        />
+                    </div>
+                    <div
+                        className="login-button"
+                        onClick={this.onLoginClick}
+                    >
+                        Login
+                    </div>
+                </div>
+                <div
+                    className="logo"
+                >
+                    <img
+                        src="logo.png"
+                        alt="Logo"
+                        width="340"
+                    />
+                </div>
+                <MomentsList Moments={moments}/>
+                <ScrollApp id="app"/>
             </div>
-          </div>
-          <center><img src="logo.png" alt="Logo" width="340"/></center>
-          <MomentsList Moments={moments}/>
-          <ScrollApp id="app"/>
-        </body>
-
-    );
-  }
+            
+        );
+    }
 }
 
 
