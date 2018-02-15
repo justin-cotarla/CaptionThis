6cce4f5808f1e659407d4b0a782676984cdc4ed8
Justin Cotarla <justin.cotarla@gmail.com>
2018-02-14 23:23:02 -0500

    fix attributes not displaying on landing page

--- frontend/src/components/Moment.js
+++ frontend/src/components/Moment.js
@@ -7,12 +7,12 @@ const Moment = props => {
         <div className="Moment-preview-container">
             <ul className="Moment-preview-layout">
                 <li>
-                    <div className="Moment-thumbnail" style={{backgroundImage: `url(${props.Image})`}} onClick={props.onClick}></div>
+                    <div className="Moment-thumbnail" style={{backgroundImage: `url(${props.image})`}} onClick={props.onClick}></div>
                 </li>
                 <li className="Moment-preview-info">
-                    <Header textSize={4} text={ props.Description } />
+                    <Header textSize={4} text={ props.description } />
                     <Header textSize={3} text={'Posted on ' + props.date } />
-                    <Header textSize={1} text={ 'Submitted by ' + props.User } />
+                    <Header textSize={1} text={ 'Submitted by ' + props.user } />
                 </li>
             </ul>
         </div>
