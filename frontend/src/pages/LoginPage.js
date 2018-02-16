fb3897fca0d8a26c2f2164b29246e120b9543c68
Constantina Roumeliotis <dina2395@hotmail.com>
2018-02-16 12:43:27 -0500

    [#74] Improving the login UI

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -3,6 +3,8 @@ import { Redirect } from 'react-router';
 import axios from 'axios';
 import Cookies from 'universal-cookie';
 import Header from '../components/Header';
+import PageHeader from '../components/PageHeader';
+import '../styles/LoginRegistrationPage.css';
 
 class LoginPage extends Component{
     constructor(props) {
@@ -34,6 +36,7 @@ class LoginPage extends Component{
 
     onSubmit = (event) => {
         event.preventDefault();
+        console.log('test');
         axios({
             url: `http://${process.env.REACT_APP_IP}:16085/api/auth/login`,
             method: 'post',
@@ -47,8 +50,10 @@ class LoginPage extends Component{
                 const cookies = new Cookies();
                 cookies.set('token', data.token);
                 this.setState({
-                    redirect: '/',
+                    redirect: '',
                 })
+            } else {
+                console.log(data);
             }
         });
     }
@@ -56,38 +61,47 @@ class LoginPage extends Component{
     render() {
         return (
             <div>
-                {this.state.redirect && <Redirect to={this.state.redirect} />}
-                <Header textSize={4} text="Login" />
+                <PageHeader />
+                <div className="login-box-container">
+                    {this.state.redirect && <Redirect to={this.state.redirect} />}
+                    <Header textSize={4} text="Login" />
 
-                <form
-                    onSubmit={this.onSubmit}
-                >
-                    <label name="userid">Username: </label>
-                    <input
-                        type="text"
-                        name="username"
-                        size="12"
-                        value={this.state.userField}
-                        onChange={this.onUserChange}
-                    />
+                    <form
+                        onSubmit={this.onSubmit}
+                    >
+                        <p>
+                        <input
+                            type="text"
+                            name="username"
+                            size="12"
+                            placeholder="Username"
+                            value={this.state.userField}
+                            onChange={this.onUserChange}
+                        /></p>
                     
-                    <label name="passid"> Password: </label>
-                    <input
-                        type="password"
-                        name="password"
-                        size="12"
-                        value={this.state.passField}
-                        onChange={this.onPassChange}
-                    />
+                        <input
+                            type="password"
+                            name="password"
+                            size="12"
+                            placeholder="Password"                       
+                            value={this.state.passField}
+                            onChange={this.onPassChange}
+                        />
 
-                    <input type="submit" value="Login" />
-                </form>
-                <label name="loginMessage">If you don't have an account, please register: </label>
-                <input
-                    type="button"
-                    value="Register"
-                    onClick={this.onRegisterClick}
-                />
+                        <input
+                            type="submit"
+                            name="login"
+                            className="login2-button"
+                        / >
+                    </form>
+                    <label name="loginMessage">If you don't have an account, please sign up! </label>
+                    <div
+                        className="registration-button"
+                        onClick={this.onRegisterClick}
+                        >
+                        Sign up
+                        </div>
+                    </div>
             </div>
         );
     }
