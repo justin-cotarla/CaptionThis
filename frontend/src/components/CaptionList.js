a506c2cda7874bee6adc54ce7244b256701609bf
tehblasian <jeremiahdavid.wreh@gmail.com>
2018-03-02 18:20:06 -0500

    [#167] This commit refactors some of the styling for the CaptionList and Caption components.

--- frontend/src/components/CaptionList.js
+++ frontend/src/components/CaptionList.js
@@ -1,17 +1,13 @@
 import React from 'react';
-import axios from 'axios';
-
 import Caption from './Caption';
-import Header from './Header';
-
 import '../styles/CaptionList.css';
 
 const CaptionList = (props) => {
     const token = props.token;
     return (
         <div className="caption-list-container">
+            { props.children }
             <ul>
-                { props.children }
                 { 
                     props.captions.map(caption => {
                         return <li key={caption.caption_id}>
