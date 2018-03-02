c174f77780f516b3fedfa0c0386064f31e6e9e45
tehblasian <jeremiahdavid.wreh@gmail.com>
2018-03-02 13:46:11 -0500

    [#167] This commit moves the state from the CaptionList into the Caption component

--- frontend/src/components/Caption.js
+++ frontend/src/components/Caption.js
@@ -1,44 +1,142 @@
 import React from 'react';
+import axios from 'axios';
 import Header from './Header';
 
 import '../styles/Caption.css';
 
-const Caption = props => {
-    let acceptStatus = '';
-    if(props.selected === -1){
-        acceptStatus = 'REJECTED';
-    } else if(props.selected === 1){
-        acceptStatus = 'ACCEPTED';
+class Caption extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            ...props.caption, 
+            token: props.token,
+        }
     }
 
-    return (
-        <div className="caption-container">
-            <ul>
-                <li>
-                    <Upvoter upvotes={props.upvotes} handler={props.voteHandler} id={props.id}/>
-                </li>
-                <li>
-                <ul className="accept-reject">
-                    { props.token && <li id="accept" value={props.id} onClick={props.acceptHandler}>Accept</li> }
-                    { props.token && <li>|</li> }
-                    { props.token && <li id="reject" value={props.id} onClick={props.acceptHandler}>Reject</li> }        
-                    <li>{acceptStatus}</li>
+    handleVote = (event) => {
+        const action = event.target.id;
+        const token = this.state.token;
+        const previousVote = this.state.user_vote;
+
+        let newVote;
+        switch (previousVote) {
+            case 0:
+                newVote = (action === '+') ? 1 : -1; 
+                break;
+            case 1:
+                newVote = (action === '+') ? 0 : -1;
+                break;
+            case -1:
+                newVote = (action === '+') ? 1 : 0;
+                break;
+            default: break;
+        }
+
+        this.setState(prevState => {
+            return {
+                total_votes: prevState.total_votes + (newVote - previousVote),
+                user_vote: newVote,
+            }
+        });
+
+        const captionid = this.state.caption_id;
+        const data = { 
+            operation: 'vote', 
+            value: newVote,
+        };
+        const headers = { 
+            'Authorization': `Bearer ${token}` 
+        };
+
+        axios({
+            method: 'post',
+            url: `http://${process.env.REACT_APP_IP}/api/captions/${captionid}`,
+            data: data,
+            headers: headers,
+        })
+        .catch(error => console.log(error)); 
+        
+        console.log(data)
+    }
+
+    handleAccept = (event) => {
+        const action = event.target.id;
+        const token = this.state.token;
+        const previousAcceptState = this.state.selected;
+
+        let newAcceptState;
+        switch (previousAcceptState) {
+            case 0:
+                newAcceptState = (action === 'accept') ? 1 : -1;
+                break;
+            case 1:
+                newAcceptState = (action === 'accept') ? 0 : -1;
+                break;
+            case -1:
+                newAcceptState = (action === 'accept') ? 1 : 0;
+                break;
+            default: break;
+        }
+
+        this.setState({
+            selected: newAcceptState,
+        });
+
+        const captionid = this.state.caption_id;
+        const data = { 
+            operation: 'select', 
+            value: newAcceptState, 
+        };
+        const headers = { 
+            'Authorization': `Bearer ${token}` 
+        };
+
+        axios({
+            method: 'post',
+            url: `http://${process.env.REACT_APP_IP}/api/captions/${captionid}`,
+            data: data,
+            headers: token ? headers : {}
+        })
+        .catch(error => console.log(error)); 
+    }
+
+    render(){
+        let acceptStatus = '';
+        if(this.state.selected === -1){
+            acceptStatus = 'REJECTED';
+        } else if(this.state.selected === 1){
+            acceptStatus = 'ACCEPTED';
+        }
+
+        return (
+            <div className="caption-container">
+                <ul>
+                    <li>
+                        <Upvoter upvotes={this.state.total_votes} voteHandler={this.handleVote} id={this.state.caption_id}/>
+                    </li>
+                    <li>
+                    <ul className="accept-reject">
+                        { this.state.token && <li id="accept" value={this.state.caption_id} onClick={this.handleAccept}>Accept</li> }
+                        { this.state.token && <li>|</li> }
+                        { this.state.token && <li id="reject" value={this.state.caption_id} onClick={this.handleAccept}>Reject</li> }        
+                        <li>{acceptStatus}</li>
+                    </ul>
+                        <Header textSize={2} text={this.state.caption}/>  
+                        <Header text={`Posted by ${this.state.user.username} on ${this.state.date_added}`}/> 
+                    </li>
                 </ul>
-                    <Header textSize={2} text={props.caption}/>  
-                    <Header text={`Posted by ${props.username} on ${props.date}`}/> 
-                </li>
-            </ul>
-        </div>
-    )
+            </div>
+        )
+    }
 }
 
 const Upvoter = props => {
     return (
         <div className="upvoter-container">
             <ul>
-                <li className="vote-ticker" id="+" value={props.id} onClick={props.handler}>+</li>
+                <li className="vote-ticker" id="+" onClick={props.voteHandler}>+</li>
                 <li className="vote-count">{props.upvotes}</li>
-                <li className="vote-ticker" id="-" value={props.id} onClick={props.handler}>-</li>
+                <li className="vote-ticker" id="-" onClick={props.voteHandler}>-</li>
             </ul>
         </div>
     )
