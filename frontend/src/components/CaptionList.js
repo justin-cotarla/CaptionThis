e58492ba9ba26421f016865a74fec05f2ff8ea6b
tehblasian <jeremiahdavid.wreh@gmail.com>
2018-03-02 13:46:11 -0500

    [#167] This commit moves the state from the CaptionList into the Caption component

--- frontend/src/components/CaptionList.js
+++ frontend/src/components/CaptionList.js
@@ -6,126 +6,25 @@ import Header from './Header';
 
 import '../styles/CaptionList.css';
 
-class CaptionList extends React.Component {
-    handleVote = (event) => {
-        console.log(this.props.token)
-        const captionid = event.target.value;
-        const action = event.target.id;
-
-        const captions = this.props.captions;
-        const caption = captions.filter(caption => caption.caption_id === captionid)[0];
-        const previousVote = caption.user_vote;
-
-        let newVote;
-        switch (previousVote) {
-            case 0:
-                newVote = (action === '+') ? 1 : -1; 
-                break;
-            case 1:
-                newVote = (action === '+') ? 0 : -1;
-                break;
-            case -1:
-                newVote = (action === '+') ? 1 : 0;
-                break;
-            default: break;
-        }
-
-        this.props.onCaptionUpdate({
-            ...caption,
-            total_votes: caption.total_votes + (newVote - previousVote),
-            user_vote: newVote,
-        });
-
-        const data = { 
-            operation: 'vote', 
-            value: newVote,
-        };
-        const headers = { 
-            'Authorization': `Bearer ${this.props.token}` 
-        };
-
-        axios({
-            method: 'post',
-            url: `http://${process.env.REACT_APP_IP}/api/captions/${captionid}`,
-            data: data,
-            headers: headers,
-        })
-        .catch(error => console.log(error)); 
-    }
-
-    handleAccept = (event) => {
-        const token = this.props.token;
-        const captionid = event.target.value;
-        const action = event.target.id;
-
-        const captions = this.props.captions;
-        const caption = captions.filter(caption => caption.caption_id === captionid)[0];
-        const previousAcceptState = caption.selected;
-
-        let newAcceptState;
-        switch (previousAcceptState) {
-            case 0:
-                newAcceptState = (action === 'accept') ? 1 : -1;
-                break;
-            case 1:
-                newAcceptState = (action === 'accept') ? 0 : -1;
-                break;
-            case -1:
-                newAcceptState = (action === 'accept') ? 1 : 0;
-                break;
-            default: break;
-        }
-
-        this.props.onCaptionUpdate({
-            ...caption,
-            selected: newAcceptState,
-        });
-
-        const data = { 
-            operation: 'select', 
-            value: newAcceptState, 
-        };
-        const headers = { 
-            'Authorization': `Bearer ${token}` 
-        };
-
-        axios({
-            method: 'post',
-            url: `http://${process.env.REACT_APP_IP}/api/captions/${captionid}`,
-            data: data,
-            headers: token ? headers : {}
-        })
-        .catch(error => console.log(error)); 
-    }
-
-    render(){
-        return (
-            <div className="caption-list-container">
-                <ul>
-                    {
-                        this.props.captions.length > 0 ? <li><Header textSize={3} text={`${this.props.captions.length} Caption${this.props.captions.length > 1 ? 's' : ''}`}/></li> 
-                        : <li><Header textSize={3} text="Looks like there's nothing here (yet) :("/></li>
-                    }
-                    { 
-                        this.props.captions.map(caption => {
-                            return <li key={caption.caption_id}>
-                                <Caption 
-                                id={caption.caption_id}
-                                upvotes={caption.total_votes}
-                                username={caption.user.username}
-                                date={caption.date_added}
-                                caption={caption.caption}
-                                selected={caption.selected}
-                                token={this.props.token}
-                                voteHandler={this.handleVote}
-                                acceptHandler={this.handleAccept}/>
-                            </li>
-                        })                  
-                    }
-                </ul>    
-            </div>
-        )
-    }
+const CaptionList = (props) => {
+    const token = props.token;
+    return (
+        <div className="caption-list-container">
+            <ul>
+                {
+                    props.captions.length > 0 ? <li><Header textSize={3} text={`${props.captions.length} Caption${props.captions.length > 1 ? 's' : ''}`}/></li> 
+                    : <li><Header textSize={3} text="Looks like there's nothing here (yet) :("/></li>
+                }
+                { 
+                    props.captions.map(caption => {
+                        return <li key={caption.caption_id}>
+                            <Caption caption={caption} token={token} />
+                        </li>
+                    })                  
+                }
+            </ul>    
+        </div>
+    )
 }
 
 export default CaptionList;
