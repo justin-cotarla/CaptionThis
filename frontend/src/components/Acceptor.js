35a3ef7476451afaaae4f8ed6ad7faebb5404889
tehblasian <jeremiahdavid.wreh@gmail.com>
2018-03-04 18:08:25 -0500

    [#176] This commit simplifies the acceptState logic and changes the prop 'token' to 'isReadOnly'

--- frontend/src/components/Acceptor.js
+++ frontend/src/components/Acceptor.js
@@ -4,21 +4,14 @@ import '../styles/Acceptor.css';
 const Acceptor = (props) => {
     const acceptState = props.status;
     let acceptStatus = '';
-    switch(acceptState){
-        case 1:
-            acceptStatus = 'ACCEPTED';
-            break;
-        case -1:
-            acceptStatus = 'REJECTED';
-            break;
-        default: break;
-    }
+    if (acceptState)
+        acceptStatus = (acceptState === 1) ? 'ACCEPTED' : 'REJECTED'; 
 
     return (
         <ul className="accept-reject">
-            { props.token && <li id="accept" value={props.caption_id} onClick={props.acceptHandler}>Accept</li> }
-            { props.token && <li>|</li> }
-            { props.token && <li id="reject" value={props.caption_id} onClick={props.acceptHandler}>Reject</li> }        
+            { props.isReadOnly && <li id="accept" value={props.caption_id} onClick={props.acceptHandler}>Accept</li> }
+            { props.isReadOnly && <li>|</li> }
+            { props.isReadOnly && <li id="reject" value={props.caption_id} onClick={props.acceptHandler}>Reject</li> }        
             <li>{acceptStatus}</li>
         </ul>
     )
