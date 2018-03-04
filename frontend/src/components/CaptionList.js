761eca52262967679c4dcf3cbfd11fd64c456ecc
tehblasian <jeremiahdavid.wreh@gmail.com>
2018-03-03 23:40:27 -0500

    [#176] This commit refactors the CaptionList so that it can render Captions that are linked to their corresponding Moment.

--- frontend/src/components/CaptionList.js
+++ frontend/src/components/CaptionList.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Caption from './Caption';
 import '../styles/CaptionList.css';
+
+const ConditionalWrap = ({condition, wrap, children}) => condition ? wrap(children) : children;
 
 const CaptionList = (props) => {
     const token = props.token;
@@ -10,8 +13,13 @@ const CaptionList = (props) => {
             <ul>
                 { 
                     props.captions.map(caption => {
-                        return <li key={caption.caption_id}>
-                            <Caption caption={caption} token={token} />
+                        return <li key={caption.caption_id} style={props.linkedToMoment ? {cursor: 'pointer'} : {}}>
+                            <ConditionalWrap
+                                condition={props.linkedToMoment}
+                                wrap={children => <Link className="linked-caption" to={`/moment/${caption.moment_id}`}>{children}</Link>}
+                            >
+                                <Caption caption={caption} token={token} />
+                            </ConditionalWrap>
                         </li>
                     })                  
                 }
