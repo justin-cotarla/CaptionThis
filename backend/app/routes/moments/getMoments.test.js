889fe685f4da0f5ec237c9ab41fb2fa7c294938b
Krish <thekrish360@hotmail.com>
2018-03-07 12:37:40 -0500

    [#191] Added testing for return values

--- backend/app/routes/moments/getMoments.test.js
+++ backend/app/routes/moments/getMoments.test.js
@@ -4,18 +4,11 @@ import getMoments from './getMoments.js';
 const request = {
     params: {
         momentid: 1,
+        userid: 1,
+        conditions: 1,
     },
     query: {
         limit: 1,
-    },
-};
-
-const badRequest = {
-    params: {
-        momentid: 1,
-    },
-    query: {
-        limit: 'a',
     },
 };
 
@@ -27,15 +20,6 @@ const emptyLimit = {
         limit: '',
     },
 };
-
-/* const undefinedLimit = {
-    params: {
-        momentid: 1,
-    },
-    query: {
-        limit
-    },
-}; */
 
 const reply = {
     response: jest.fn().mockImplementation(() => ({
@@ -51,27 +35,46 @@ describe('/api/getMoments endpoint', () => {
     it('Successfully get moments', () => {
         databaseUtil.sendQuery = jest.fn(() => new Promise((resolve) => {
             resolve({
-                rows: [{}],
+                rows: [{
+                    MOMENT_ID: 1,
+                    USER_ID: 1,
+                    USERNAME: 'test',
+                    IMG_URL: 'test',
+                    DESCRIPTION: 'test',
+                    DATE_ADDED: 1,
+                }],
                 fields: {},
             });
         }));
         return getMoments.handler(request, reply)
             .then(() => {
-                expect(reply.response.mock.calls[0][0].code).toBe(1);
+                expect(reply.response.mock.calls[0][0]).toEqual({
+                    code: 1,
+                    moments: [{
+                        moment_id: 1,
+                        user: {
+                            user_id: 1,
+                            username: 'test',
+                        },
+                        img: 'test',
+                        description: 'test',
+                        date_added: 1,
+                    }],
+                });
             });
     });
-    it('Limit is non-integer', () => {
-        getMoments.handler(badRequest, reply);
-        expect(reply.response.mock.calls[0][0].code).toBe(2);
+    it('User ID is pushed into values', () => {
+        getMoments.handler(request, reply);
+        expect(request.params.userid).toBe(1);
+    });
+    it('ID pushed into conditions', () => {
+        getMoments.handler(request, reply);
+        expect(request.params.userid).toBe(1);
     });
     it('Limit is empty string', () => {
         getMoments.handler(emptyLimit, reply);
         expect(emptyLimit.query.limit).toBe('');
     });
-    /* it('Limit is undefined', () => {
-        getMoments.handler(undefinedLimit, reply);
-        expect(emptyLimit.query.limit).toBe(20);
-    }); */
     it('Handle unknown error', () => {
         databaseUtil.sendQuery = jest.fn(() => new Promise(() => {
             throw new Error();
