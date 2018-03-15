b99fb90ab65fe64058ed0f8b51dcfffe915e1d16
ZacharyBys <zachary.bys@gmail.com>
2018-03-15 19:48:43 -0400

    [#195] hover now works on colored + and - (and some fixes)

--- frontend/src/components/CaptionVotes.js
+++ frontend/src/components/CaptionVotes.js
@@ -2,18 +2,16 @@ import React from 'react';
 import '../styles/CaptionVotes.css';
 
 const CaptionVotes = props => {
-    var upVote = <li className="vote-ticker" id="+" onClick={props.voteHandler}>+</li>
-    var downVote = <li className="vote-ticker" id="-" onClick={props.voteHandler}>-</li>
+    let upVote = <li className="vote-ticker" id="+" onClick={props.voteHandler}>+</li>
+    let downVote = <li className="vote-ticker" id="-" onClick={props.voteHandler}>-</li>
 
     switch (props.vote_value) {      
         case 1:
             upVote = <li className="vote-up" id="+" onClick={props.voteHandler}>+</li>
             break;
-
         case -1:
             downVote = <li className="vote-down" id="-" onClick={props.voteHandler}>-</li>
             break;
-        
         default:
             break;
     }
