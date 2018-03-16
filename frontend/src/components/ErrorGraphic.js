a49698fddcad646c9c4502b400b1bf941a9d5c70
ZacharyBys <zachary.bys@gmail.com>
2018-03-15 23:12:45 -0400

    [#134] Error graphic component created and added to the Landing Page

---
+++ frontend/src/components/ErrorGraphic.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import '../styles/Error.css';
+
+class Error extends Component{
+    render() {
+        return (
+            <div className="error_container">
+                <img className="error_image" src={`http://${process.env.REACT_APP_IP}/res/error.png`} alt="Error"/>
+                <h1 className="error_message">{this.props.error_message} </h1>
+            </div>
+        )
+    }   
+}
+
+export default Error;
