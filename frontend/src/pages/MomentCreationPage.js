1bed88d993656655e91bc7626d65460b923f3fe4
Yasmine Ghassemi <ghassemiyasmine@gmail.com>
2018-03-18 14:22:55 -0400

    [#246] Remove unused Header component.

--- frontend/src/pages/MomentCreationPage.js
+++ frontend/src/pages/MomentCreationPage.js
@@ -2,10 +2,7 @@ import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
 import Cookies from 'universal-cookie';
 import NavBar from '../components/NavBar';
-
 import '../styles/CreateMoment.css';
-import Header from '../components/Header';
-
 
 
 class MomentCreation extends Component {
@@ -61,14 +58,11 @@ class MomentCreation extends Component {
                     src={`http://${process.env.REACT_APP_IP}/res/uploadMoment.png`}
                 />
                 </div>
-
                 <div className = "CreateMoment">
                     {this.state.redirect && <Redirect to={this.state.redirect} />}
                     <form onSubmit={this.handleSubmit} encType="multipart/form-data" noValidate>
-
                         <div className = "content-container">
-
-                        <input type="file" name="file" id="file" className="inputfile" onChange={this.handleChange} required/>
+                            <input type="file" name="file" id="file" className="inputfile" onChange={this.handleChange} required/>
                             <label for="file">Choose Moment
                                 <img
                                     alt="inputFile"
@@ -82,18 +76,14 @@ class MomentCreation extends Component {
                               Description
                             </div>
                             <div className="Wrapper">
-
                                 <div className="Input">
                                     <textarea type="text" id="input" name="description" className="Input-text" row="4" cols="50" placeholder="Add a Description to your Moment"/>
                                     <label for="input" className="Input-label">This will help users come up with your ideal caption!</label>
                                 </div>
-                            </div>
-
+                            </div>    
                         </div>
                         <div className = "content-container">
-                            <input type="submit" className="button">
-                            </input>
-
+                            <input type="submit" className="button"></input>
                         </div>
                     </form>
                 </div>
