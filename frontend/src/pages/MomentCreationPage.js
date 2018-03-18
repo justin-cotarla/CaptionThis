71154741b4d7f11b204df3f9a2ebd3635eff1cc0
Yasmine Ghassemi <ghassemiyasmine@gmail.com>
2018-03-18 16:24:17 -0400

    [#246]Remove extra divs

--- frontend/src/pages/MomentCreationPage.js
+++ frontend/src/pages/MomentCreationPage.js
@@ -15,7 +15,6 @@ class MomentCreation extends Component {
         }
     }
 
-
     componentDidMount() {
         const cookies = new Cookies();
         const token = cookies.get('token');
@@ -53,9 +52,10 @@ class MomentCreation extends Component {
             <div>
                 <NavBar user={this.state.user}/>
                 <div className = "createMoment-header">
+
                 <img
                     alt="UploadMoment"
-                    src={`http://${process.env.REACT_APP_IP}/res/uploadMoment.png`}
+                    src={`http://${process.env.REACT_APP_IP}/res/uploadyourmoment.png`}
                 />
                 </div>
                 <div className = "CreateMoment">
@@ -66,25 +66,21 @@ class MomentCreation extends Component {
                             <label for="file">Choose Moment
                                 <img
                                     alt="inputFile"
-                                    src={`http://${process.env.REACT_APP_IP}/res/inputfile.png`}
+                                    src={`http://${process.env.REACT_APP_IP}/res/uploadicon.png`}
                                 />
                             </label>
                             <div id="filename"></div>
-                        </div>
-                        <div className = "content-container">
-                            <div className = "createMoment-description">
+                            <label className = "createMoment-description">
                               Description
-                            </div>
+                            </label>
                             <div className="Wrapper">
                                 <div className="Input">
                                     <textarea type="text" id="input" name="description" className="Input-text" row="4" cols="50" placeholder="Add a Description to your Moment"/>
                                     <label for="input" className="Input-label">This will help users come up with your ideal caption!</label>
                                 </div>
-                            </div>    
+                            </div>
                         </div>
-                        <div className = "content-container">
                             <input type="submit" className="button"></input>
-                        </div>
                     </form>
                 </div>
             </div>
