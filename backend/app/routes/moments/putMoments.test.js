ef8c0e515ac194800ee50b442168f41df5d718cd
Derek Yu <derekyu624@hotmail.com>
2018-03-19 23:11:18 -0400

    [#237] Refactor response codes for putMoments.js and its tests.

--- backend/app/routes/moments/putMoments.test.js
+++ backend/app/routes/moments/putMoments.test.js
@@ -1,6 +1,12 @@
 import AWS from 'aws-sdk';
 import databaseUtil from '../../utility/DatabaseUtil';
 import putMoments from './putMoments.js';
+import {
+    GOOD,
+    UNAUTHORIZED,
+    INVALID_INPUT,
+    UNKNOWN_ERROR,
+} from '../../utility/ResponseCodes';
 
 const request = {
     auth: {
@@ -25,6 +31,20 @@ const request = {
 const emptyAuthRequest = {
     auth: {},
     payload: {},
+};
+
+const emptyFileRequest = {
+    auth: {
+        credentials: {
+            user: {
+                id: 1,
+                username: 'test',
+            },
+        },
+    },
+    payload: {
+        description: 'test',
+    },
 };
 
 const reply = {
@@ -56,13 +76,18 @@ describe('/api/moments endpoint', () => {
 
         return putMoments.handler(request, reply)
             .then(() => {
-                expect(reply.response.mock.calls[0][0].code).toBe(1);
+                expect(reply.response.mock.calls[0][0].code).toBe(GOOD.code);
             });
     });
 
     it('Request with no authentication', () => {
         putMoments.handler(emptyAuthRequest, reply);
-        expect(reply.response.mock.calls[0][0].code).toBe(4);
+        expect(reply.response.mock.calls[0][0].code).toBe(UNAUTHORIZED.code);
+    });
+
+    it('Handles request with empty file upload', () => {
+        putMoments.handler(emptyFileRequest, reply);
+        expect(reply.response.mock.calls[0][0].code).toBe(INVALID_INPUT.code);
     });
 
     it('Handle unknown error', () => {
@@ -74,7 +99,7 @@ describe('/api/moments endpoint', () => {
 
         return putMoments.handler(request, reply)
             .then(() => {
-                expect(reply.response.mock.calls[0][0].code).toBe(3);
+                expect(reply.response.mock.calls[0][0].code).toBe(UNKNOWN_ERROR.code);
             });
     });
 });
