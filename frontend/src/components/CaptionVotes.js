8b89134ff728ba97e46c180062b880f87f9ca72c
tehblasian <jeremiahdavid.wreh@gmail.com>
2018-03-24 11:54:36 -0400

    [#313] This commit refactors the vote tickers so that they are identified by className and not by id

--- frontend/src/components/CaptionVotes.js
+++ frontend/src/components/CaptionVotes.js
@@ -2,15 +2,15 @@ import React from 'react';
 import '../styles/CaptionVotes.css';
 
 const CaptionVotes = props => {
-    let upVote = <li className="vote-ticker" id="+" onClick={props.voteHandler}>+</li>
-    let downVote = <li className="vote-ticker" id="-" onClick={props.voteHandler}>-</li>
+    let upVote = <li className="vote-ticker-plus" id="+" onClick={props.voteHandler}>+</li>
+    let downVote = <li className="vote-ticker-minus" id="-" onClick={props.voteHandler}>-</li>
 
     switch (props.vote_value) {      
         case 1:
-            upVote = <li className="vote-up" id="+" onClick={props.voteHandler}>+</li>
+            upVote = <li className="vote-ticker-plus" style={{color: '#1DE28F'}} id="+" onClick={props.voteHandler}>+</li>
             break;
         case -1:
-            downVote = <li className="vote-down" id="-" onClick={props.voteHandler}>-</li>
+            downVote = <li className="vote-ticker-minus" style={{color: '#f44b42'}} id="-" onClick={props.voteHandler}>-</li>
             break;
         default:
             break;
