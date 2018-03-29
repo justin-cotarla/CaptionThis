4d99e985e151e9ddeb97ec1740afbfd5de33c3f2
tehblasian <jeremiahdavid.wreh@gmail.com>
2018-03-29 17:11:12 -0400

    [#342] This commit deleted the CaptionEditor file, and moves all of its functionality into the Caption component

--- frontend/src/components/CaptionList.js
+++ frontend/src/components/CaptionList.js
@@ -116,7 +116,7 @@ class CaptionList extends React.Component {
                                             && (momentCreatorId === user.id) // The logged-on user created the moment
                                             && (user.id !== caption.user.id) // The logged-on user did not create the caption
                                         } 
-                                        canEdit={user && isInteractive && (user.id === caption.user.user_id)}
+                                        canEdit={user && isInteractive && (user.id === caption.user.id)}
                                         token={token} />
                                 </ConditionalWrap>
                             </li>
