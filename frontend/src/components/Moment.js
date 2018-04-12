5e7fbf202ea311730a771f935b8d809265d8d26c
Derek Yu <derekyu624@hotmail.com>
2018-04-11 23:45:51 -0400

    [#369] Add moment description editing on the frontend.

--- frontend/src/components/Moment.js
+++ frontend/src/components/Moment.js
@@ -5,6 +5,8 @@ import { Redirect } from 'react-router';
 import ConditionalWrap from './ConditionalWrap';
 import DeleteModal from './DeleteModal';
 import '../styles/Moment.css';
+import { editMoment } from '../util/ApiUtil';
+import LoadingDots from './LoadingDots';
 
 class Moment extends Component {
     constructor(props){
@@ -12,8 +14,16 @@ class Moment extends Component {
         this.state = {
             toHome: false,
             showAuthModal: false,
+            showDeleteModal: false,
+            editing: false,
+            editor: {
+                newDesc: props.description,
+                isEditing: false,
+                editError: '',
+            },
+            token: props.token,
         };
-        
+
     };
 
     deleteMoment = () => {
@@ -22,8 +32,59 @@ class Moment extends Component {
         });
     }
 
+    onEditClick = event => {
+        event.preventDefault();
+        this.setState({ editing: true });
+    }
+
+    onEdit = event => {
+        event.preventDefault();
+        const newDescription = event.target.value;
+        const { editor } = this.state;
+        editor.newDesc = newDescription;
+        this.setState({ editor });
+    }
+
+    onCancelEdit = () => {
+        const { editor } = this.state;
+        editor.newDesc = this.props.description;
+        this.setState({
+            editor,
+            editing: false,
+        });
+    }
+
+    onSave = event => {
+        event.preventDefault();
+        const { token, editor } = this.state;
+        const { newDesc } = editor;
+        const momentId = this.props.momentId;
+
+        editor.isEditing = true;
+        this.setState({ editor });
+
+        editMoment({ token, momentId, newDesc })
+        .then(() => {
+            editor.isEditing = false;
+            this.setState(prevState => {
+                return {
+                    ...prevState,
+                    editing: false,
+                    editor,
+                }
+            });
+        })
+        .catch(error => {
+            editor.isEditing = false;
+            editor.editError = 'There was a problem editing your moment...';
+            this.setState({ editor });
+        });
+    }
+
     render() {
         const props = this.props;
+        const { editing, token } = this.state;
+        const { newDesc, isEditing, editError } = this.state.editor;
         return (
             <div
             className="Moment-preview-container"
@@ -35,12 +96,49 @@ class Moment extends Component {
                     onClose={() => this.setState({ showDeleteModal: false })}/>
             {this.state.toHome && <Redirect to={'/'} />}
             { (props.currentUser ? props.currentUser.username === props.user.username : false) && <h1 className="delete-button" onClick={this.deleteMoment}>X</h1> }
+            {
+                (props.currentUser ? props.currentUser.username === props.user.username : false) && !editing &&
+                <img
+                    className="edit-button"
+                    src={`http://${process.env.REACT_APP_IP}/res/edit.png`}
+                    alt="edit"
+                    onClick={this.onEditClick}
+                />
+            }
                 <ConditionalWrap
                     condition={this.props.momentId}
                     wrap={children =>  <Link to={`/moment/${props.momentId}`}>{children}</Link>}
                 >
                     <img className="Moment-thumbnail" src={props.image} alt="moment"/>
-                    <h1 className="top-caption">{ props.description }</h1>
+                    {
+                        editing ?
+                        <div className="moment-editor-container">
+                            <textarea
+                                ref={(textarea) => this.Textarea = textarea}
+                                className="moment-editor-input"
+                                type="text"
+                                value={newDesc}
+                                onChange={this.onEdit}/>
+                            {
+                                editError && <h1 style={{ color: '#ff5e56', fontWeight: 200, fontSize: '16px', marginTop: '0.2em' }}>{editError}</h1>
+                            }
+                            <button
+                                className="moment-editor-btn"
+                                style={{ marginRight: '0.5em'}}
+                                onClick={this.onSave}
+                                disabled={((token) && (newDesc.length === 0 || props.description === newDesc)) || isEditing}>Save</button>
+                            <button
+                                className="moment-editor-btn"
+                                onClick={this.onCancelEdit}
+                                disabled={isEditing}>
+                                Cancel
+                            </button>
+                            {
+                                isEditing && <div className="is-editing"><LoadingDots/></div>
+                            }
+                        </div>
+                        : <h1 className="top-caption">{ props.description }</h1>
+                    }
                 </ConditionalWrap>
                     <h1 style={{fontSize: '20px'}}>Posted {timeAgo(props.date)}</h1>
                     {
